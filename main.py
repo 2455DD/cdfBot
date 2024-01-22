4eb7aa33d401a6965a989eefac7379b75dfec57a
cannedfish <cannedfishsan@gmail.com>
2024-01-22 23:38:07 +0800

    abstract main() in main

--- main.py
+++ main.py
@@ -12,19 +12,24 @@ from graia.ariadne.model import Group
 from graia.broadcast import Broadcast
 from graia.saya import Saya
 
-bcc = create(Broadcast)
 
-saya = create(Saya)
-app = Ariadne(
-    connection=config(
-        1
+def main():
+    bcc = create(Broadcast)
+
+    saya = create(Saya)
+    app = Ariadne(
+        connection=config(
+            1
+        )
     )
-)
 
-with saya.module_context():
-    for module_info in pkgutil.iter_modules(["modules"]):
-        if module_info.name.startswith("_"): # 不导入`_`开头模组
-            continue
-        saya.require(f"modules.{module_info.name}")
-        
-app.launch_blocking()
+    with saya.module_context():
+        for module_info in pkgutil.iter_modules(["modules"]):
+            if module_info.name.startswith("_"): # 不导入`_`开头模组
+                continue
+            saya.require(f"modules.{module_info.name}")
+            
+    app.launch_blocking()
+
+if __name__ == "__main__":
+    main()
