bdf69e91711ec3cbce7788f3f1073f09432c8308
cannedfish <cannedfishsan@gmail.com>
2024-07-15 22:59:07 +0800

    feat: :sparkles: introduce MediaParser
    实现了MediaParser，用于进行多媒体内容存储

--- plugins/media_parser/__init__.py
+++ plugins/media_parser/__init__.py
@@ -2,13 +2,14 @@ import os
 from alicebot import Plugin,ConfigModel
 from alicebot.event import MessageEvent
 from alicebot.adapter.cqhttp import CQHTTPMessage,CQHTTPMessageSegment
-from typing import List
+from typing import List,Union
 from structlog.stdlib import get_logger
 import sqlite3
-import aiohttp
+import httpx
 import aiofiles
 from PIL import Image
 import imagehash
+import videohash
 
 logger = get_logger()
 
@@ -27,7 +28,11 @@ class MediaParser(Plugin[MessageEvent,sqlite3.Connection,MediaParserConfig]):
     """
     priority = 2
     def __init_state__(self) -> sqlite3.Connection | None:
-        conn = sqlite3.connect(self.config.db_path,timeout=5)
+        if not os.path.exists(self.config.db_path):
+            logger.error("数据库无法打开，跳过链接")
+            return
+        else:    
+            conn = sqlite3.connect(self.config.db_path,timeout=5)
         return conn
     
     async def _save_metadata_to_db(self,path:str,type:str):
@@ -42,14 +47,13 @@ class MediaParser(Plugin[MessageEvent,sqlite3.Connection,MediaParserConfig]):
                 raise ValueError(f"{type} 不被支持")
             
     async def _handle_multimedia_segment(self,seg:CQHTTPMessageSegment):
-        
         # TODO: 数据库处理
         if seg.type == "image":
             url = seg["url"]
         elif seg.type == "video":
             url = seg["url"]
         elif seg.type == "file":
-            raise NotImplementedError("文件形式的消息暂不允许")
+            raise NotImplementedError("文件形式的消息暂不允许") #TODO: 实现文件形式内容
         elif seg.type == "text":
             return
         else:
@@ -57,33 +61,74 @@ class MediaParser(Plugin[MessageEvent,sqlite3.Connection,MediaParserConfig]):
         
         
         
-        file_name = self.config.download_root_path + f"/{seg.type}/{seg['file']}"
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url) as resp:
-                if resp.status == 200:
-                    async with aiofiles.open(file_name,mode="wb") as f:
-                        f.write(await resp.read())
-                        
+        file_name = f"{self.config.download_root_path}/{seg.type}/{seg['file']}"
+        
+        if not os.path.exists(self.config.download_root_path):
+            os.makedirs(self.config.download_root_path)
+        
+        async with httpx.AsyncClient() as client:
+            resp = await client.get(url)
+            if resp.status_code == 200:
+                async with aiofiles.open(file_name,mode="wb") as f:
+                    await f.write(await resp.aread())
         
         match seg.type:
             case "image":
-                hash = await imagehash.crop_resistant_hash(Image.open(file_name),min_segment_size=500, segmentation_image_size=1000)
+                orginal_hash = imagehash.whash(Image.open(file_name))
+                hash = str(orginal_hash)
+            case "video":
+                hash = str(videohash.VideoHash(url=url)) #TODO
+            case "file":
+                hash = None #TODO: 实现文件形式内容
+            case "_":
+                raise ValueError(f"{type} 不被支持")
+
+        logger.debug(hash)
         
     
+    async def parse_forward_msgs(self,forward_msg:CQHTTPMessage) ->  List[CQHTTPMessageSegment]:
+        raw_forward_msgs:List[CQHTTPMessage] = await self.event.adapter.call_api("get_forward_msg"
+                                                        ,id=forward_msg[0].get("id"))
+        
+        node_segs:List[CQHTTPMessageSegment] = list()
+        
+        for node in raw_forward_msgs["messages"]: # 结点列表中不同节点
+            for node_msg_txt in node["content"]:
+                match node_msg_txt["type"]:
+                    case "image":
+                        node_segs += CQHTTPMessageSegment(type="image",
+                            data={
+                                "file": node_msg_txt["data"]["file"],
+                                "url": node_msg_txt["data"]["url"],
+                                "type": None,
+                            },)
+                    case "video":
+                        node_segs += CQHTTPMessageSegment(type="video",
+                            data={
+                                "file": node_msg_txt["data"]["file"],
+                                "url": node_msg_txt["data"]["url"],
+                                "type": None,
+                            },)
+                    case "forward":
+                        logger.debug(f"{forward_msg[0].get("id")}：递归解析合并{node_msg_txt["data"]["id"]}")
+                        temp_seg_node = CQHTTPMessage(CQHTTPMessageSegment(type="forward",
+                                                             data={
+                                                                 "id":str(node_msg_txt["data"]["id"])
+                                                             }
+                                                    ))
+                        
+                        node_segs.extend(await self.parse_forward_msgs(temp_seg_node))
+        
+        return node_segs
+    
     async def handle(self) -> None:
-        if self.event.message.type == "forward":
-            forward_msg:CQHTTPMessage = self.event.message
-            
-            forward_msgs:List[CQHTTPMessage] = await self.event.adapter.call_api("get_forward_msg"
-                                                                    ,id=forward_msg[0].get("id"))
-            for node in forward_msgs: # 结点列表中不同节点
-                for node_msg in node[0].get("content"):
-                    for node_seg in node_msg:
-                        try:
-                            await self._handle_multimedia_segment(node_seg)
-                        except NotImplementedError:
-                            logger.info()
-
+        if self.event.message[0].type == "forward":
+            segs = await self.parse_forward_msgs(self.event.message)
+            for seg in segs:
+                try:
+                    await self._handle_multimedia_segment(seg)
+                except NotImplementedError:
+                    logger.warn(f"处理{seg.type}类别方法仍未实现")
         else:
             for node_seg in self.event.message:
                     try:
