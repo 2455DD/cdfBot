f9e47f5a0fa779041143693f267c7eb99d34ef09
cannedfish <cannedfishsan@gmail.com>
2025-05-21 00:58:23 +0800

    docs: :memo: 为MediaParserSqlHelper添加docstring

--- plugins/media_parser/media_sql_helper.py
+++ plugins/media_parser/media_sql_helper.py
@@ -1,22 +1,27 @@
+""" MediaParser中SQL通信相关代码子模块 """
+
 import sqlite3
-from typing import *
 from pathlib import Path
 from structlog.stdlib import get_logger
 
 logger = get_logger()
 
 class MediaParserSqlHelper():
-    def __init__(self,db_path:str|Path) -> None:
-        if isinstance(db_path,str):
-            db_path = Path(db_path) 
-        assert db_path.parent.exists(),"媒体数据库父目录不存在"
-        assert not db_path.is_dir(),"媒体数据库路径是目录"
-        self.db_path:Path = db_path
+    """媒体转换器数据库助手类"""
+
+    def __init__(self, db_path: str | Path) -> None:
+        if isinstance(db_path, str):
+            db_path = Path(db_path)
+        assert db_path.parent.exists(), "媒体数据库父目录不存在"
+        assert not db_path.is_dir(), "媒体数据库路径是目录"
+        self.db_path: Path = db_path
         self.init_db()
-        
+
     def init_db(self):
+        """初始化数据库，特别是创建表结构
+        """
         conn = sqlite3.connect(self.db_path)
-        
+
         conn.executescript('''
             CREATE TABLE IF NOT EXISTS "Image" (
             "id" INTEGER NOT NULL,
@@ -90,79 +95,131 @@ class MediaParserSqlHelper():
             ''')
         conn.autocommit = False
         self.conn = conn
-    
-    #---------------- Insert -------------------------
-    def insert_image(self,image_name:str,image_hash:str,image_path:str) :
+
+    # ---------------- Insert -------------------------
+    def insert_image(self, image_name: str, image_hash: str, image_path: str):
+        """插入图片记录
+
+        Args:
+            image_name (str): 图片文件名
+            image_hash (str): 图片哈希值，目前存储imagehash的whash值
+            image_path (str): 目标路径
+
+        Raises:
+            sqlite3.OperationalError: 数据库操作错误
+        """
         try:
             cur = self.conn.cursor()
             cur.execute('''
                         INSERT INTO Image(file_name,file_path,hash)
                         VALUES (?,?,?)
-                        ''',(image_name,image_path,image_hash))
+                        ''', (image_name, image_path, image_hash))
             self.conn.commit()
         except sqlite3.OperationalError as err:
             self.conn.rollback()
             raise err
         finally:
             cur.close()
-              
-    def insert_video(self,video_name:str,video_hash:str,video_path:str):
+
+    def insert_video(self, video_name: str, video_hash: str, video_path: str):
+        """插入视频记录
+
+        Args:
+            video_name (str): 视频文件名
+            video_hash (str): 视频哈希值，目前用videohash库计算
+            video_path (str): 目标路径
+
+        Raises:
+            sqlite3.OperationalError: 数据库操作错误
+        """
         try:
             cur = self.conn.cursor()
             cur.execute('''
                         INSERT INTO Video(file_name,file_path,hash)
                         VALUES (?,?,?)
-                        ''',(video_name,video_path,video_hash))
-            self.conn.commit() 
+                        ''', (video_name, video_path, video_hash))
+            self.conn.commit()
         except sqlite3.OperationalError as err:
             self.conn.rollback()
             raise err
         finally:
             cur.close()
-            
-    #---------------- Query ------------------------
-  
-    def is_image_exists(self,image_hash:str)->bool:
+
+    # ---------------- Query ------------------------
+
+    def is_image_exists(self, image_hash: str) -> bool:
+        """检查图片是否存在, 避免重复
+
+        Args:
+            image_hash (str): 查询hash值
+
+        Raises:
+            sqlite3.OperationalError: 数据库错误
+
+        Returns:
+            bool: `True`时存在
+        """
         try:
             cur = self.conn.cursor()
             cur.execute('''
                         SELECT 1 FROM Image WHERE Image.hash = ?
-                        ''',(image_hash,))
+                        ''', (image_hash,))
             self.conn.commit()
             if cur.fetchone() is not None:
                 return True
             else:
-                return False  
+                return False
         except sqlite3.OperationalError as err:
             self.conn.rollback()
             raise err
         finally:
             cur.close()
 
+    def is_video_exists(self, video_hash: str) -> bool:
+        """检查视频是否存在, 避免重复
 
-    def is_video_exists(self,video_hash:str)->bool:
+        Args:
+            video_hash (str): 查询hash值
+
+        Raises:
+            sqlite3.OperationalError: 数据库错误
+
+        Returns:
+            bool: `True`时存在
+        """
         try:
             cur = self.conn.cursor()
             cur.execute('''
                         SELECT 1 FROM Video WHERE Video.hash = ?
-                        ''',(video_hash,))
+                        ''', (video_hash,))
             self.conn.commit()
             if cur.fetchone() is not None:
                 return True
             else:
-                return False  
+                return False
         except sqlite3.OperationalError as err:
             self.conn.rollback()
             raise err
         finally:
             cur.close()
-            
-    def get_image_path_by_hash(self,image_hash:str)->str:
+
+    def get_image_path_by_hash(self, image_hash: str) -> str:
+        """获取数据库中图片路径
+
+        Args:
+            image_hash (str): 查询hash值
+
+        Raises:
+            sqlite3.OperationalError: 数据库错误
+
+        Returns:
+            str: 数据库中图片路径
+        """
         try:
             cur = self.conn.cursor()
             cur.execute('''
                         SELECT Image.file_path FROM Image WHERE Image.hash = ?
-                        ''',(image_hash,))
+                        ''', (image_hash,))
             self.conn.commit()
             result = cur.fetchone()
             return result[0]
@@ -172,13 +229,23 @@ class MediaParserSqlHelper():
         finally:
             cur.close()
 
+    def get_video_path_by_hash(self, video_hash: str) -> str:
+        """获取数据库中视频路径
 
-    def get_video_path_by_hash(self,video_hash:str)->str:
+        Args:
+            video_hash (str): 查询hash值
+
+        Raises:
+            sqlite3.OperationalError: 数据库错误
+
+        Returns:
+            str: 数据库中视频路径
+        """
         try:
             cur = self.conn.cursor()
             cur.execute('''
                         SELECT Video.file_path FROM Video WHERE Video.hash = ?
-                        ''',(video_hash,))
+                        ''', (video_hash,))
             self.conn.commit()
             result = cur.fetchone()
             return result[0]
@@ -187,34 +254,49 @@ class MediaParserSqlHelper():
             raise err
         finally:
             cur.close()
-            
-    #-------------- 删除------------------
-    def delete_image_record(self,image_hash:str)->str:
+
+    # -------------- 删除------------------
+    def delete_image_record(self, image_hash: str):
+        """删除目标图片记录
+
+        Args:
+            image_hash (str): 目标图片的hash值
+
+        Raises:
+            sqlite3.OperationalError: 数据库错误
+        """
         try:
             cur = self.conn.cursor()
             cur.execute('''
                         DELETE FROM Image WHERE Image.hash = ?
-                        ''',(image_hash,))
+                        ''', (image_hash,))
             self.conn.commit()
         except sqlite3.OperationalError as err:
             self.conn.rollback()
             raise err
         finally:
             cur.close()
-    
-    def delete_video_record(self,video_hash:str):
+
+    def delete_video_record(self, video_hash: str):
+        """删除目标视频记录
+
+        Args:
+            video_hash (str): 目标视频的hash值
+
+        Raises:
+            sqlite3.OperationalError: 数据库错误
+        """
         try:
             cur = self.conn.cursor()
             cur.execute('''
                         DELETE FROM Video WHERE Video.hash = ?
-                        ''',(video_hash,))
+                        ''', (video_hash,))
             self.conn.commit()
         except sqlite3.OperationalError as err:
             self.conn.rollback()
             raise err
         finally:
             cur.close()
-            
-    
+
 if __name__ == "__main__":
     MediaParserSqlHelper("./.develop_assets/media.db")
