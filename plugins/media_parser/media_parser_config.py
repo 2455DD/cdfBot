711879b3d9a18a5cd1ebab65745428ca08986dc7
cannedfish <cannedfishsan@gmail.com>
2025-07-30 19:01:28 +0800

    feat(media_parser): :wrench: add s3-related config item to MediaParserConfig
    including s3_endpoint,s3_access_key and s3_secret_key

--- plugins/media_parser/media_parser_config.py
+++ plugins/media_parser/media_parser_config.py
@@ -2,6 +2,7 @@
 
 from pathlib import Path
 
+from httpx import URL
 from alicebot import ConfigModel
 
 
@@ -9,5 +10,9 @@ class MediaParserConfig(ConfigModel):
     """媒体转化器设置，包括数据库路径、下载地址等等
     """
     __config_name__ = "media_parser"
-    db_path:Path = Path("db/media.db")
-    download_root_path:Path = Path("assets/downloads")
+    db_path: Path = Path("db/media.db")
+    download_root_path: Path = Path("assets/downloads")
+
+    s3_endpoint: URL = URL("s3.eu-central-1.s4.mega.io")
+    s3_access_key: str = None
+    s3_secret_key: str = None
